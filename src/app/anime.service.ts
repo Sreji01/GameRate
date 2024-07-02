daa261a6fd169f0def13f17b4a292c2b78002052
Sreji01 <marko.srejic01@gmail.com>
2024-07-02 11:11:47 +0200

    Sredjena anime komponenta i explore stranica, dodata watchlist stranica i anime service, implementirano da se animei dovlace iz baze podataka.

---
+++ src/app/anime.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import {Anime} from "./anime.model";
+import {HttpClient} from "@angular/common/http";
+import {map} from "rxjs";
+
+interface AnimeData{
+  id: string,
+  title: string,
+  year: number,
+  description: string,
+  imageUrl: string,
+  posterUrl:string
+}
+@Injectable({
+  providedIn: 'root'
+})
+export class AnimeService {
+
+  animes!: Anime[];
+  dbUrl: string = "https://anime-app-1efe0-default-rtdb.europe-west1.firebasedatabase.app/animes.json"
+  constructor(private http: HttpClient) { }
+
+  getAnimes(){
+    return this.http.get<{[key: string]: AnimeData }>(this.dbUrl).
+    pipe(map( (animesData) => {
+      const animes: Anime[] = [];
+
+      for (const key in animesData){
+        if (animesData.hasOwnProperty(key)){
+          animes.push({
+            id: key,
+            title: animesData[key].title,
+            year: animesData[key].year,
+            imageUrl: animesData[key].imageUrl,
+            posterUrl: animesData[key].posterUrl,
+            description: animesData[key].description
+          });
+        }
+      }
+      this.animes = animes;
+      return animes;
+    }));
+  }
+}
