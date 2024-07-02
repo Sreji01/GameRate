4f74c36b4b372ecf37731820d88c8099e44da0b7
Sreji01 <marko.srejic01@gmail.com>
2024-07-02 09:46:40 +0200

    Prvi commit.

---
+++ src/app/anime/anime.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-anime',
+  templateUrl: './anime.component.html',
+  styleUrls: ['./anime.component.scss'],
+})
+export class AnimeComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
