bdf397f6326abdcdad4bf1845f6e5b95997ddbec
Borkan01 <sarkovicstide@gmail.com>
2024-07-04 19:49:16 +0200

    Dodata header komponenta, promenjena tema aplikacije i pozadina, dodat tap-bar u celoj aplikaciji.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,10 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import {HttpClientModule} from "@angular/common/http";
+import {HeaderComponent} from "./header/header.component";
 
 @NgModule({
-  declarations: [AppComponent],
+    declarations: [AppComponent, HeaderComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
